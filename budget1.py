4b30f9f383e14cf22a220c5667b397cc4dc4acc0
101010 <1337@haxor.2600>
2013-05-24 15:22:51 -0700

    Test Commit

---
+++ budget1.py
@@ -0,0 +1,16 @@
+# LPTHW Exercise 3 EC - budget1.py
+
+# Category Header
+print "Monthly Expenditures:"
+
+# Listing the monthly expenditures
+print "Rent: ", 1500.0
+print "Student Loan: ", 90.0
+print "Car Insuranse: ", 95.0
+print "Food: ", 200.0
+print "Gas: ", 100.0
+print "Cable: ", 30.0
+print "Cell Phone: ", 110.0
+
+# Output Monthly Totals
+print "Total Monthly Expenditures: ", 1500.0 + 90.0 + 95.0 + 200.0 + 100.0 + 30.0 + 110.0
